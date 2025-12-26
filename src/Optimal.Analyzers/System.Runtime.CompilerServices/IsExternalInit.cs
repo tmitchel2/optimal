c85ae4d091fd85d778335fed37fd48230f4f3c69
Tom Mitchell <tmitchel2@googlemail.com>
2025-12-26 12:42:38 +0000

    Add analyzer and tests

---
+++ src/Optimal.Analyzers/System.Runtime.CompilerServices/IsExternalInit.cs
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Small Trading Company Ltd (Destash.com).
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+#pragma warning disable IDE0130
+namespace System.Runtime.CompilerServices
+#pragma warning restore IDE0130
+{
+    internal static class IsExternalInit { }
+}
---
+++ src/Optimal.Tests/OptimalTests.cs
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Small Trading Company Ltd (Destash.com).
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Optimal.Tests
+{
+    [TestClass]
+    public sealed class OptimalTests
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+        }
+    }
+}
