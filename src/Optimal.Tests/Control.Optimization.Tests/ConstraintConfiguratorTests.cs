bd43a87a06e16f728879e1177ba12483437f2a20
Tom Mitchell <tmitchel2@googlemail.com>
2026-01-11 15:56:54 +0000

    Update test

---
+++ src/Optimal.Tests/Control.Optimization.Tests/ConstraintConfiguratorTests.cs
@@ -0,0 +1,278 @@
+/**
+ * Copyright (c) Small Trading Company Ltd (Destash.com).
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+#pragma warning disable CA1861 // Prefer static readonly fields - not applicable for lambda captures
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Optimal.Control.Collocation;
+using Optimal.Control.Core;
+using Optimal.Control.Optimization;
+using Optimal.NonLinear.Constrained;
+
+namespace Optimal.Control.Optimization.Tests
+{
+    [TestClass]
+    public sealed class ConstraintConfiguratorTests
+    {
+        private static readonly double[] s_zeroState1D = new[] { 0.0 };
+        private static readonly double[] s_oneState1D = new[] { 1.0 };
+        private static readonly double[] s_zeroState2D = new[] { 0.0, 0.0 };
+        private static readonly double[] s_oneZeroState2D = new[] { 1.0, 0.0 };
+
+        [TestMethod]
+        public void AddDefectConstraintsAddsCorrectNumberOfConstraints()
+        {
+            var problem = CreateSimpleProblem();
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var optimizer = new AugmentedLagrangianOptimizer();
+            optimizer.WithInitialPoint(new double[transcription.DecisionVectorSize]);
+
+            ConstraintConfigurator.AddDefectConstraints(
+                problem, grid, transcription, 5, hasAnalyticalGradients: false, optimizer);
+
+            // 5 segments * 1 state dim = 5 defect constraints
+            // Just verify no exception is thrown - constraint count is internal
+            Assert.IsNotNull(optimizer);
+        }
+
+        [TestMethod]
+        public void AddDefectConstraintsHandlesMultipleStates()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(2)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithInitialCondition(s_zeroState2D)
+                .WithFinalCondition(s_oneZeroState2D)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { x[1], u[0] };
+                    var gradients = new double[2][];
+                    gradients[0] = new double[4] { 0.0, 1.0, 0.0, 0.0 };
+                    gradients[1] = new double[2] { 0.0, 1.0 };
+                    return (value, gradients);
+                });
+
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var optimizer = new AugmentedLagrangianOptimizer();
+            optimizer.WithInitialPoint(new double[transcription.DecisionVectorSize]);
+
+            ConstraintConfigurator.AddDefectConstraints(
+                problem, grid, transcription, 5, hasAnalyticalGradients: true, optimizer);
+
+            // 5 segments * 2 state dims = 10 defect constraints
+            Assert.IsNotNull(optimizer);
+        }
+
+        [TestMethod]
+        public void AddBoundaryConstraintsAddsInitialStateConstraints()
+        {
+            var problem = CreateSimpleProblem();
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var optimizer = new AugmentedLagrangianOptimizer();
+            optimizer.WithInitialPoint(new double[transcription.DecisionVectorSize]);
+
+            ConstraintConfigurator.AddBoundaryConstraints(problem, transcription, 5, optimizer);
+
+            Assert.IsNotNull(optimizer);
+        }
+
+        [TestMethod]
+        public void AddBoundaryConstraintsHandlesNoInitialState()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    return (value, gradients);
+                });
+
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var optimizer = new AugmentedLagrangianOptimizer();
+            optimizer.WithInitialPoint(new double[transcription.DecisionVectorSize]);
+
+            // Should not throw
+            ConstraintConfigurator.AddBoundaryConstraints(problem, transcription, 5, optimizer);
+
+            Assert.IsNotNull(optimizer);
+        }
+
+        [TestMethod]
+        public void AddBoxConstraintsDoesNothingWhenNoBounds()
+        {
+            var problem = CreateSimpleProblem();
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var optimizer = new AugmentedLagrangianOptimizer();
+            optimizer.WithInitialPoint(new double[transcription.DecisionVectorSize]);
+
+            // Should not throw
+            ConstraintConfigurator.AddBoxConstraints(problem, transcription, 5, optimizer);
+
+            Assert.IsNotNull(optimizer);
+        }
+
+        [TestMethod]
+        public void AddBoxConstraintsAddsControlBounds()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithInitialCondition(s_zeroState1D)
+                .WithFinalCondition(s_oneState1D)
+                .WithControlBounds(new[] { -1.0 }, new[] { 1.0 })
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    return (value, gradients);
+                });
+
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var optimizer = new AugmentedLagrangianOptimizer();
+            optimizer.WithInitialPoint(new double[transcription.DecisionVectorSize]);
+
+            ConstraintConfigurator.AddBoxConstraints(problem, transcription, 5, optimizer);
+
+            Assert.IsNotNull(optimizer);
+        }
+
+        [TestMethod]
+        public void AddBoxConstraintsAddsStateBounds()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithInitialCondition(s_zeroState1D)
+                .WithFinalCondition(s_oneState1D)
+                .WithStateBounds(new[] { -5.0 }, new[] { 5.0 })
+                .WithControlBounds(new[] { -1.0 }, new[] { 1.0 })
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    return (value, gradients);
+                });
+
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var optimizer = new AugmentedLagrangianOptimizer();
+            optimizer.WithInitialPoint(new double[transcription.DecisionVectorSize]);
+
+            ConstraintConfigurator.AddBoxConstraints(problem, transcription, 5, optimizer);
+
+            Assert.IsNotNull(optimizer);
+        }
+
+        [TestMethod]
+        public void AddPathConstraintsDoesNothingWhenNoConstraints()
+        {
+            var problem = CreateSimpleProblem();
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var optimizer = new AugmentedLagrangianOptimizer();
+            optimizer.WithInitialPoint(new double[transcription.DecisionVectorSize]);
+
+            // Should not throw
+            ConstraintConfigurator.AddPathConstraints(problem, grid, transcription, 5, optimizer);
+
+            Assert.IsNotNull(optimizer);
+        }
+
+        [TestMethod]
+        public void AddPathConstraintsAddsConstraintsAtEachNode()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithInitialCondition(s_zeroState1D)
+                .WithFinalCondition(s_oneState1D)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    return (value, gradients);
+                })
+                .WithPathConstraint((x, u, t) =>
+                {
+                    // x[0] <= 2.0 expressed as x[0] - 2.0 <= 0
+                    var value = x[0] - 2.0;
+                    double[]? gradients = null;
+                    return (value, gradients!);
+                });
+
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var optimizer = new AugmentedLagrangianOptimizer();
+            optimizer.WithInitialPoint(new double[transcription.DecisionVectorSize]);
+
+            ConstraintConfigurator.AddPathConstraints(problem, grid, transcription, 5, optimizer);
+
+            Assert.IsNotNull(optimizer);
+        }
+
+        [TestMethod]
+        public void AddDefectConstraintsWithAnalyticalGradientsWorks()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithInitialCondition(s_zeroState1D)
+                .WithFinalCondition(s_oneState1D)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    gradients[0] = new[] { 0.0 };
+                    gradients[1] = new[] { 1.0 };
+                    return (value, gradients);
+                });
+
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var optimizer = new AugmentedLagrangianOptimizer();
+            optimizer.WithInitialPoint(new double[transcription.DecisionVectorSize]);
+
+            ConstraintConfigurator.AddDefectConstraints(
+                problem, grid, transcription, 5, hasAnalyticalGradients: true, optimizer);
+
+            Assert.IsNotNull(optimizer);
+        }
+
+        private static ControlProblem CreateSimpleProblem()
+        {
+            return new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithInitialCondition(s_zeroState1D)
+                .WithFinalCondition(s_oneState1D)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    gradients[0] = new[] { 0.0 };
+                    gradients[1] = new[] { 1.0 };
+                    return (value, gradients);
+                });
+        }
+    }
+}
---
+++ src/Optimal.Tests/Control.Optimization.Tests/ObjectiveFunctionFactoryTests.cs
@@ -0,0 +1,357 @@
+/**
+ * Copyright (c) Small Trading Company Ltd (Destash.com).
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+#pragma warning disable CA1861 // Prefer static readonly fields - not applicable for lambda captures
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Optimal.Control.Collocation;
+using Optimal.Control.Core;
+using Optimal.Control.Optimization;
+
+namespace Optimal.Control.Optimization.Tests
+{
+    [TestClass]
+    public sealed class ObjectiveFunctionFactoryTests
+    {
+        private static readonly double[] s_zeroState1D = new[] { 0.0 };
+        private static readonly double[] s_oneState1D = new[] { 1.0 };
+
+        [TestMethod]
+        public void ComputeTotalCostReturnsZeroForNoCosts()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithInitialCondition(s_zeroState1D)
+                .WithFinalCondition(s_oneState1D)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    gradients[0] = new[] { 0.0 };
+                    gradients[1] = new[] { 1.0 };
+                    return (value, gradients);
+                });
+
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var z = new double[transcription.DecisionVectorSize];
+
+            var cost = ObjectiveFunctionFactory.ComputeTotalCost(problem, transcription, z);
+
+            Assert.AreEqual(0.0, cost);
+        }
+
+        [TestMethod]
+        public void ComputeTotalCostIncludesRunningCost()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithInitialCondition(s_zeroState1D)
+                .WithFinalCondition(s_oneState1D)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    gradients[0] = new[] { 0.0 };
+                    gradients[1] = new[] { 1.0 };
+                    return (value, gradients);
+                })
+                .WithRunningCost((x, u, t) =>
+                {
+                    var value = 1.0; // Constant running cost
+                    var gradients = new double[3];
+                    return (value, gradients);
+                });
+
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var z = new double[transcription.DecisionVectorSize];
+
+            var cost = ObjectiveFunctionFactory.ComputeTotalCost(problem, transcription, z);
+
+            // Cost should be approximately 1.0 (integral of 1 over [0,1])
+            Assert.IsTrue(cost > 0.5 && cost < 1.5, $"Running cost should be approximately 1.0, was {cost}");
+        }
+
+        [TestMethod]
+        public void ComputeTotalCostIncludesTerminalCost()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithInitialCondition(s_zeroState1D)
+                .WithFinalCondition(s_oneState1D)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    gradients[0] = new[] { 0.0 };
+                    gradients[1] = new[] { 1.0 };
+                    return (value, gradients);
+                })
+                .WithTerminalCost((x, t) =>
+                {
+                    var value = 10.0; // Constant terminal cost
+                    var gradients = new double[2];
+                    return (value, gradients);
+                });
+
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var z = new double[transcription.DecisionVectorSize];
+
+            var cost = ObjectiveFunctionFactory.ComputeTotalCost(problem, transcription, z);
+
+            Assert.AreEqual(10.0, cost);
+        }
+
+        [TestMethod]
+        public void ComputeTotalCostSumsBothCosts()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithInitialCondition(s_zeroState1D)
+                .WithFinalCondition(s_oneState1D)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    gradients[0] = new[] { 0.0 };
+                    gradients[1] = new[] { 1.0 };
+                    return (value, gradients);
+                })
+                .WithRunningCost((x, u, t) =>
+                {
+                    var value = 1.0;
+                    var gradients = new double[3];
+                    return (value, gradients);
+                })
+                .WithTerminalCost((x, t) =>
+                {
+                    var value = 10.0;
+                    var gradients = new double[2];
+                    return (value, gradients);
+                });
+
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var z = new double[transcription.DecisionVectorSize];
+
+            var cost = ObjectiveFunctionFactory.ComputeTotalCost(problem, transcription, z);
+
+            // Running cost ~1.0 + terminal cost 10.0
+            Assert.IsTrue(cost > 10.5 && cost < 11.5, $"Total cost should be ~11.0, was {cost}");
+        }
+
+        [TestMethod]
+        public void CanUseAnalyticalGradientsForCostsReturnsTrueWhenGradientsProvided()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    gradients[0] = new[] { 0.0 };
+                    gradients[1] = new[] { 1.0 };
+                    return (value, gradients);
+                })
+                .WithRunningCost((x, u, t) =>
+                {
+                    var value = u[0] * u[0];
+                    var gradients = new double[3]; // StateDim + ControlDim + 1 = 3
+                    gradients[0] = 0.0;
+                    gradients[1] = 2.0 * u[0];
+                    gradients[2] = 0.0;
+                    return (value, gradients);
+                });
+
+            var result = ObjectiveFunctionFactory.CanUseAnalyticalGradientsForCosts(problem);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanUseAnalyticalGradientsForCostsReturnsFalseWhenGradientsTooShort()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    return (value, gradients);
+                })
+                .WithRunningCost((x, u, t) =>
+                {
+                    var value = u[0] * u[0];
+                    var gradients = new double[1]; // Too short - needs 3
+                    return (value, gradients);
+                });
+
+            var result = ObjectiveFunctionFactory.CanUseAnalyticalGradientsForCosts(problem);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CanUseAnalyticalGradientsForCostsReturnsFalseWhenGradientsNull()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    return (value, gradients);
+                })
+                .WithRunningCost((x, u, t) =>
+                {
+                    var value = u[0] * u[0];
+                    double[]? gradients = null;
+                    return (value, gradients!);
+                });
+
+            var result = ObjectiveFunctionFactory.CanUseAnalyticalGradientsForCosts(problem);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CanUseAnalyticalGradientsForCostsReturnsTrueForNoCosts()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    return (value, gradients);
+                });
+
+            var result = ObjectiveFunctionFactory.CanUseAnalyticalGradientsForCosts(problem);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanUseAnalyticalGradientsForCostsChecksTerminalCost()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    return (value, gradients);
+                })
+                .WithTerminalCost((x, t) =>
+                {
+                    var value = x[0];
+                    var gradients = new double[2]; // StateDim + 1 = 2
+                    gradients[0] = 1.0;
+                    gradients[1] = 0.0;
+                    return (value, gradients);
+                });
+
+            var result = ObjectiveFunctionFactory.CanUseAnalyticalGradientsForCosts(problem);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ComputeObjectiveGradientReturnsCorrectSize()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithInitialCondition(s_zeroState1D)
+                .WithFinalCondition(s_oneState1D)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    gradients[0] = new[] { 0.0 };
+                    gradients[1] = new[] { 1.0 };
+                    return (value, gradients);
+                })
+                .WithRunningCost((x, u, t) =>
+                {
+                    var value = u[0] * u[0];
+                    var gradients = new double[3];
+                    gradients[0] = 0.0;
+                    gradients[1] = 2.0 * u[0];
+                    gradients[2] = 0.0;
+                    return (value, gradients);
+                });
+
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var z = new double[transcription.DecisionVectorSize];
+
+            var gradient = ObjectiveFunctionFactory.ComputeObjectiveGradient(problem, grid, transcription, z);
+
+            Assert.AreEqual(z.Length, gradient.Length);
+        }
+
+        [TestMethod]
+        public void ComputeObjectiveGradientUsesNumericalGradientsWhenNoAnalytical()
+        {
+            var problem = new ControlProblem()
+                .WithStateSize(1)
+                .WithControlSize(1)
+                .WithTimeHorizon(0.0, 1.0)
+                .WithInitialCondition(s_zeroState1D)
+                .WithFinalCondition(s_oneState1D)
+                .WithDynamics((x, u, t) =>
+                {
+                    var value = new[] { u[0] };
+                    var gradients = new double[2][];
+                    return (value, gradients);
+                })
+                .WithRunningCost((x, u, t) =>
+                {
+                    var value = u[0] * u[0];
+                    double[]? gradients = null;
+                    return (value, gradients!);
+                });
+
+            var grid = new CollocationGrid(0.0, 1.0, 5);
+            var transcription = new ParallelHermiteSimpsonTranscription(problem, grid);
+            var z = new double[transcription.DecisionVectorSize];
+            // Set some controls to non-zero
+            for (var k = 0; k <= 5; k++)
+            {
+                z[k * 2 + 1] = 0.5; // Control at each node
+            }
+
+            var gradient = ObjectiveFunctionFactory.ComputeObjectiveGradient(problem, grid, transcription, z);
+
+            Assert.AreEqual(z.Length, gradient.Length);
+            // Gradient should be non-zero for control variables
+        }
+    }
+}
